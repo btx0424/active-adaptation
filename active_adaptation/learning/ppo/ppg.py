1a47d3c59547c95da20510f66bf94d3ab7d7d0e0
btx0424 <btx0424@outlook.com>
2024-02-13 07:11:26 +0000

    ppg model loss & rnn

--- active_adaptation/learning/ppo/ppg.py
+++ active_adaptation/learning/ppo/ppg.py
@@ -6,14 +6,23 @@ import torch.distributions as D
 from torchrl.data import CompositeSpec, TensorSpec
 from torchrl.modules import ProbabilisticActor
 from torchrl.envs.transforms import CatTensors
-from tensordict.nn import TensorDictModule, TensorDictSequential
+from tensordict.nn import (
+    TensorDictModule, 
+    TensorDictSequential, 
+    TensorDictModuleBase,
+    utils
+)
 
 from hydra.core.config_store import ConfigStore
 from dataclasses import dataclass
+import einops
 
 from ..utils.valuenorm import ValueNorm1
 from ..modules.distributions import IndependentNormal
 from .common import *
+from .ppo_rnn import GRU
+import functools
+
 
 @dataclass
 class PPGConfig:
@@ -23,6 +32,7 @@ class PPGConfig:
     aux_epochs: int = 6
     beta_clone: float = 1.
 
+    rnn_actor: bool = False
     num_minibatches: int = 16
     lr: float = 5e-4
     clip_param: float = 0.2
@@ -30,8 +40,68 @@ class PPGConfig:
 
 cs = ConfigStore.instance()
 cs.store(name="ppg", node=PPGConfig, group="algo")
+cs.store(name="ppg_rnn", node=PPGConfig(rnn_actor=True), group="algo")
 
-class PPGPolicy:
+
+class GRU(nn.Module):
+    def __init__(
+        self, 
+        input_size, 
+        hidden_size, 
+        allow_none: bool = False,
+        burn_in: bool = False
+    ) -> None:
+        super().__init__()
+        self.gru = nn.GRUCell(input_size, hidden_size)
+        self.ln = nn.LayerNorm(hidden_size)
+        self.allow_none = allow_none
+        self.burn_in = burn_in
+
+    def forward(self, x: torch.Tensor, is_init: torch.Tensor, hx: torch.Tensor):
+        if x.ndim == 2: # single step
+
+            N = x.shape[0]
+            if hx is None and self.allow_none:
+                hx = torch.zeros(N, self.gru.hidden_size, device=x.device)
+            assert (hx[is_init.squeeze()] == 0.).all()
+            output = hx = self.gru(x, hx)
+            output = self.ln(output)
+            return output, hx
+
+        elif x.ndim == 3: # multi-step
+
+            N, T = x.shape[:2]
+            if hx is None and self.allow_none:
+                hx = torch.zeros(N, self.gru.hidden_size, device=x.device)
+            else:
+                hx = hx[:, 0]
+            output = []
+            reset = 1. - is_init.float().reshape(N, T, 1)
+            for i, x_t, reset_t in zip(range(T), x.unbind(1), reset.unbind(1)):
+                hx = self.gru(x_t, hx * reset_t)
+                if self.burn_in and i < T // 4:
+                    hx = hx.detach()
+                output.append(hx)
+            output = torch.stack(output, dim=1)
+            output = self.ln(output)
+            return output, einops.repeat(hx, "b h -> b t h", t=T)
+
+
+class GRUModule(nn.Module):
+    def __init__(self, dim: int):
+        super().__init__()
+        self.mlp = make_mlp([256, 128])
+        self.gru = GRU(128, hidden_size=128, allow_none=False)
+        self.out = make_mlp([dim])
+    
+    def forward(self, x, is_init, hx):
+        x = self.mlp(x)
+        x, hx = self.gru(x, is_init, hx)
+        x = self.out(x)
+        return x, hx.contiguous()
+
+
+class PPGPolicy(TensorDictModuleBase):
 
     def __init__(
         self,
@@ -47,7 +117,7 @@ class PPGPolicy:
 
         self.entropy_coef = 0.001
         self.clip_param = self.cfg.clip_param
-        self.critic_loss_fn = nn.HuberLoss(delta=10, reduction="none")
+        self.critic_loss_fn = nn.HuberLoss(delta=10)
         self.action_dim = action_spec.shape[-1]
 
         self.gae = GAE(0.99, 0.95).to(self.device)
@@ -55,31 +125,67 @@ class PPGPolicy:
 
         fake_input = observation_spec.zero()
 
-        self.actor: ProbabilisticActor = ProbabilisticActor(
-            TensorDictSequential(
-                TensorDictModule(make_mlp([256, 256, 128]), [OBS_KEY], ["_feature"]),
-                TensorDictModule(nn.LazyLinear(1), ["_feature"], ["value_actor"]),
-                TensorDictModule(Actor(self.action_dim), ["_feature"], ["loc", "scale"]),
-            ),
-            in_keys=["loc", "scale"],
-            out_keys=[ACTION_KEY],
-            distribution_class=IndependentNormal,
-            return_log_prob=True
-        ).to(self.device)
+        with utils._set_auto_make_functional(False):
+            if self.cfg.rnn_actor:
+                self.actor: ProbabilisticActor = ProbabilisticActor(
+                    TensorDictSequential(
+                        TensorDictModule(GRUModule(128), [OBS_KEY, "is_init", "hx"], ["_feature_actor", ("next", "hx")]),
+                        TensorDictModule(nn.LazyLinear(1), ["_feature_actor"], ["value_actor"]),
+                        TensorDictModule(Actor(self.action_dim), ["_feature_actor"], ["loc", "scale"]),
+                    ),
+                    in_keys=["loc", "scale"],
+                    out_keys=[ACTION_KEY],
+                    distribution_class=IndependentNormal,
+                    return_log_prob=True
+                ).to(self.device)
+                fake_input["hx"] = torch.zeros(fake_input.shape[0], 128, device=self.device)
 
-        self.critic = TensorDictSequential(
-            CatTensors([OBS_KEY, OBS_PRIV_KEY], "full_obs", del_keys=False),
-            TensorDictModule(
-                nn.Sequential(make_mlp([256, 256, 128]), nn.LazyLinear(1),),
-                ["full_obs"], ["state_value"]
-            ),
-        ).to(self.device)
+                from torchrl.envs.transforms.transforms import TensorDictPrimer
+                from torchrl.data import UnboundedContinuousTensorSpec
+                def make_tensordict_primer():
+                    num_envs = observation_spec.shape[0]
+                    return TensorDictPrimer({
+                        "hx": UnboundedContinuousTensorSpec((num_envs, 128), device=self.device)
+                    })
+                self.make_tensordict_primer = make_tensordict_primer
+
+            else:
+                self.actor: ProbabilisticActor = ProbabilisticActor(
+                    TensorDictSequential(
+                        TensorDictModule(make_mlp([256, 256, 128]), [OBS_KEY], ["_feature_actor"]),
+                        TensorDictModule(nn.LazyLinear(1), ["_feature_actor"], ["value_actor"]),
+                        TensorDictModule(Actor(self.action_dim), ["_feature_actor"], ["loc", "scale"]),
+                    ),
+                    in_keys=["loc", "scale"],
+                    out_keys=[ACTION_KEY],
+                    distribution_class=IndependentNormal,
+                    return_log_prob=True
+                ).to(self.device)
+
+            self.critic = TensorDictSequential(
+                CatTensors([OBS_KEY, OBS_PRIV_KEY], "full_obs", del_keys=False),
+                TensorDictModule(
+                    nn.Sequential(make_mlp([256, 256, 128]), nn.LazyLinear(1),),
+                    ["full_obs"], ["state_value"]
+                ),
+            ).to(self.device)
+
+        self.action_enc = make_mlp([32]).to(self.device)
+        self.dynamics = make_mlp([128, 128]).to(self.device)
 
         self.actor(fake_input)
         self.critic(fake_input)
+        self.action_enc(fake_input[ACTION_KEY])
+        self.dynamics(
+            torch.cat([fake_input["_feature_actor"], self.action_enc(fake_input[ACTION_KEY])], -1)
+        )
 
         self.opt = torch.optim.Adam(
-            list(self.actor.parameters()) + list(self.critic.parameters()),
+            [
+                {"params": self.actor.parameters()},
+                {"params": self.critic.parameters()},
+                {"params": self.dynamics.parameters()}
+            ],
             lr=self.cfg.lr
         )
 
@@ -88,6 +194,8 @@ class PPGPolicy:
         else:
             self.actor.apply(init_)
             self.critic.apply(init_)
+            self.action_enc.apply(init_)
+            self.dynamics.apply(init_)
         
         self.train_iter = 0
 
@@ -96,23 +204,25 @@ class PPGPolicy:
         return tensordict
     
     def train_op(self, tensordict: TensorDictBase):
+        assert tensordict.device == self.device
         infos = {}
 
         infos_policy = []
         for epoch in range(self.cfg.ppo_epochs):
             if epoch == 0 or self.cfg.recompute_adv:
                 self._compute_advantage(tensordict)
-            for minibatch in make_batch(tensordict, self.cfg.num_minibatches):
+            for minibatch in make_batch(tensordict, self.cfg.num_minibatches, self.cfg.train_every):
                 infos_policy.append(self._update(minibatch))
         
         infos.update(collect_info(infos_policy))
         
-        if self.train_iter % 16 == 0 and self.cfg.aux_epochs > 0:
+        if self.cfg.aux_epochs > 0 and self.train_iter % 16 == 0:
             infos_aux = []
             with torch.no_grad():
                 self.actor.get_dist_params(tensordict)
+                self.actor(tensordict["next"])
             for epoch in range(self.cfg.aux_epochs):
-                for minibatch in make_batch(tensordict, self.cfg.num_minibatches):
+                for minibatch in make_batch(tensordict, self.cfg.num_minibatches, self.cfg.train_every):
                     infos_aux.append(self._update_aux(minibatch))
             infos.update(collect_info(infos_aux))
         
@@ -121,8 +231,9 @@ class PPGPolicy:
 
     @torch.no_grad()
     def _compute_advantage(self, tensordict: TensorDict, subtract_mean: bool=False):
-        values = self.critic(tensordict)["state_value"]
-        next_values = self.critic(tensordict["next"])["state_value"]
+        shape = (*tensordict.shape, -1)
+        values = self.critic(tensordict.view(-1))["state_value"].reshape(shape)
+        next_values = self.critic(tensordict["next"].view(-1))["state_value"].reshape(shape)
 
         rewards = tensordict[REWARD_KEY]
         dones = tensordict[DONE_KEY]
@@ -143,6 +254,7 @@ class PPGPolicy:
         tensordict.set("ret", ret)
         return tensordict
     
+    # @functools.partial(torch.compile, mode="reduce-overhead")
     def _update(self, tensordict: TensorDictBase):
         dist = self.actor.get_dist(tensordict)
         log_probs = dist.log_prob(tensordict[ACTION_KEY])
@@ -155,21 +267,15 @@ class PPGPolicy:
         policy_loss = - torch.mean(torch.min(surr1, surr2)) * self.action_dim
         entropy_loss = - self.entropy_coef * entropy
 
-        b_values = tensordict["state_value"]
         b_returns = tensordict["ret"]
         values = self.critic(tensordict)["state_value"]
-        values_clipped = b_values + (values - b_values).clamp(
-            -self.clip_param, self.clip_param
-        )
-        value_loss_clipped = self.critic_loss_fn(b_returns, values_clipped)
-        value_loss_original = self.critic_loss_fn(b_returns, values)
-        value_loss = torch.max(value_loss_original, value_loss_clipped).mean()
+        value_loss = self.critic_loss_fn(b_returns, values)
 
         loss = policy_loss + entropy_loss + value_loss
         self.opt.zero_grad()
         loss.backward()
-        actor_grad_norm = nn.utils.clip_grad.clip_grad_norm_(self.actor.parameters(), 10)
-        critic_grad_norm = nn.utils.clip_grad.clip_grad_norm_(self.critic.parameters(), 10)
+        actor_grad_norm = nn.utils.clip_grad_norm_(self.actor.parameters(), 10)
+        critic_grad_norm = nn.utils.clip_grad_norm_(self.critic.parameters(), 10)
         self.opt.step()
         explained_var = 1 - F.mse_loss(values, b_returns) / b_returns.var()
         return TensorDict({
@@ -193,7 +299,13 @@ class PPGPolicy:
         aux_value_loss_actor = F.mse_loss(values_actor, b_returns)
         aux_value_loss = F.mse_loss(values_critic, b_returns)
 
-        loss = aux_value_loss_actor + aux_value_loss + self.cfg.beta_clone * kl
+        feature_pred = self.dynamics(
+            torch.cat([tensordict["_feature_actor"], self.action_enc(tensordict[ACTION_KEY])], -1)
+        )
+        feature_next = tensordict["next", "_feature_actor"]
+        aux_model_loss = F.mse_loss(feature_pred, feature_next)
+
+        loss = aux_model_loss + aux_value_loss_actor + aux_value_loss + self.cfg.beta_clone * kl
         self.opt.zero_grad()
         loss.backward()
         grad_norm = nn.utils.clip_grad.clip_grad_norm_(self.actor.parameters(), 10)
@@ -203,5 +315,6 @@ class PPGPolicy:
             "aux_kl": kl,
             "aux_value_loss": aux_value_loss,
             "aux_value_loss_actor": aux_value_loss_actor,
+            "aux_model_loss": aux_model_loss,
             "grad_norm": grad_norm
         }, [])
