092b7f600e12ebb489c298777d09fdfe5888c3fb
btx0424 <btx0424@outlook.com>
2024-05-25 03:29:49 +0800

    [rew] feet_swing

--- active_adaptation/envs/mdp/rewards/humanoid.py
+++ active_adaptation/envs/mdp/rewards/humanoid.py
@@ -28,25 +28,36 @@ class feet_swing(Reward):
         self.feet_id = self.asset.find_bodies(feet_names)[0]
         self.phase: torch.Tensor = self.asset.data.phase
         self.command_manager = self.env.command_manager
+
+        self.feet_vel_buf = torch.zeros(self.num_envs, 2, 3, 4, device=self.device)
     
+    def update(self):
+        self.feet_vel_buf[..., 1:] = self.feet_vel_buf[..., :-1]
+        self.feet_vel_buf[..., 0] = self.asset.data.body_lin_vel_w[:, self.feet_id]
+
+    def reset(self, env_ids):
+        self.feet_vel_buf[env_ids] = 0.
+
     def compute(self) -> torch.Tensor:
-        feet_linvel = self.asset.data.body_lin_vel_w[:, self.feet_id]
+        feet_linvel = self.feet_vel_buf.mean(-1)
         swing_vel = torch.zeros_like(feet_linvel)
         swing_vel[:] = self.command_manager._command_linvel.unsqueeze(1)
         phase_sin = self.phase.sin()
-        swing_vel[:, 0] *= (phase_sin > +0.1).unsqueeze(1)
-        swing_vel[:, 1] *= (phase_sin < -0.1).unsqueeze(1)
+        swing_vel[:, 0] *= (phase_sin > +0.1).float().unsqueeze(1)
+        swing_vel[:, 1] *= (phase_sin < -0.1).float().unsqueeze(1)
         swing_vel = quat_rotate(yaw_quat(self.asset.data.root_quat_w).unsqueeze(1), swing_vel)
         # reward = torch.exp(- 2 * (feet_linvel - swing_vel).abs().sum(-1)).sum(1, True)
-        reward = (normalize(swing_vel) * feet_linvel).sum(-1).clip(max=1.).sum(1, True)
-        return 0.5 * reward.reshape(self.num_envs, 1)
+        reward = (normalize(swing_vel) * feet_linvel).sum(-1).sum(1, True)
+        return reward.reshape(self.num_envs, 1)
     
     def debug_draw(self):
         feet_pos = self.asset.data.body_pos_w[:, self.feet_id]
+        feet_linvel = self.feet_vel_buf.mean(-1)
         swing_vel = torch.zeros_like(feet_pos)
         swing_vel[:] = self.command_manager._command_linvel.unsqueeze(1)
-        swing_vel[:, 0] *= (self.phase.sin() > +0.1).unsqueeze(1)
-        swing_vel[:, 1] *= (self.phase.sin() < -0.1).unsqueeze(1)
+        swing_vel[:, 0] *= (self.phase.sin() > +0.1).float().unsqueeze(1)
+        swing_vel[:, 1] *= (self.phase.sin() < -0.1).float().unsqueeze(1)
         swing_vel = quat_rotate(yaw_quat(self.asset.data.root_quat_w).unsqueeze(1), swing_vel)
         self.env.debug_draw.vector(feet_pos.reshape(-1, 3), swing_vel.reshape(-1, 3))
+        self.env.debug_draw.vector(feet_pos.reshape(-1, 3), feet_linvel.reshape(-1, 3), color=(1., 0., 0.2, 1.))
 
