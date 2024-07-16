92d2bc75e9376cbb7d851c41d4ce4d14b1b65d53
btx0424 <btx0424@outlook.com>
2024-07-16 22:39:14 +0800

    cleanup

--- active_adaptation/learning/__init__.py
+++ active_adaptation/learning/__init__.py
@@ -4,20 +4,6 @@ from .sac import SAC
 from .hbc import HBC
 
 ALGOS = {
-    "ppo": PPOPolicy,
-    "ppo_asy": PPOAsyPolicy,
-    "ppo_dual": PPODualPolicy,
-    "ppo_rnn": PPORNNPolicy,
-    "ppo_roa": PPOROAPolicy,
-    "ppo_guided": PPOGuidedPolicy,
-    "ppo_adapt": PPOAdaptPolicy,
-    "ppo_ji": PPOJi,
-    "ppo_stoch": PPOStochPolicy,
-    "ppo_mask": PPOMaskPolicy,
-    "ppo_him": PPOHIMPolicy,
-    "ppo_priv": PPOPrivPolicy,
-    # "ppo_rma": PPORMAPolicy,
-    "ppg": PPGPolicy,
     "sac": SAC,
     "hbc": HBC,
     "bc": BCPolicy
