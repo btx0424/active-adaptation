06a81fd9b104ec46d5c67b431e61ee14fffb7451
btx0424 <btx0424@outlook.com>
2024-08-05 16:25:06 +0800

    eval_run

--- scripts/eval_run.py
+++ scripts/eval_run.py
@@ -7,15 +7,25 @@ import argparse
 
 from omegaconf import OmegaConf
 from omni.isaac.lab.app import AppLauncher
-from scripts.helpers import make_env_policy, evaluate
+from scripts.play import main as play_main
+from scripts.eval import main as eval_main
+
+play = play_main.__wrapped__
+eval = eval_main.__wrapped__
 
 FILE_PATH = os.path.dirname(__file__)
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--run_path", type=str)
+    parser.add_argument("-r", "--run_path", type=str)
     parser.add_argument("--task", type=str, default=None)
-    parser.add_argument("--play", action="store_true", default=False)
+    parser.add_argument("-p", "--play", action="store_true", default=False)
+    # whether to override terrain and command
+    parser.add_argument("-t", "--terrain", action="store_true", default=False)
+    parser.add_argument("-c", "--command", action="store_true", default=False)
+    
+    parser.add_argument("-e", "--export", action="store_true", default=False)
+    parser.add_argument("-v", "--video", action="store_true", default=False)
     args = parser.parse_args()
 
     api = wandb.Api()
@@ -33,59 +43,57 @@ def main():
             file.download(root, replace=True)
         elif file.name == "config.yaml":
             file.download(root, replace=True)
+    
+    def sort_by_time(file):
+        number_str = file.name[:-3].split("_")[-1]
+        if number_str == "final":
+            return 100000
+        else:
+            return int(number_str)
+
+    checkpoints.sort(key=sort_by_time)
     checkpoint = checkpoints[-1]
     print(f"Downloading {checkpoint.name}")
     checkpoint.download(replace=True)
-    
+
     # `run.config` does not preserve order of the keys
     # so we need to manually load the config file :(
-    if os.path.exists(os.path.join(root, "config.yaml")):
-        cfg = OmegaConf.load(os.path.join(root, "config.yaml"))
-        for k, v in run.config.items():
-            cfg[k] = cfg[k]["value"]
-    else:
-        cfg = OmegaConf.load(os.path.join(root, "files", "cfg.yaml"))
+    # if os.path.exists(os.path.join(root, "config.yaml")):
+    #     cfg = OmegaConf.load(os.path.join(root, "config.yaml"))
+    #     for k, v in run.config.items():
+    #         cfg[k] = cfg[k]["value"]
+    # else:
+    cfg = OmegaConf.load(os.path.join(root, "files", "cfg.yaml"))
     OmegaConf.set_struct(cfg, False)
 
     cfg["checkpoint_path"] = os.path.join(os.path.dirname(__file__), checkpoint.name)
     cfg["vecnorm"] = "eval"
-    # cfg["algo"]["phase"] = "adapt"
-
-    if args.play:
-        cfg["app"]["headless"] = False
-        cfg["task"]["num_envs"] = 16
-    else:
-        cfg["task"]["num_envs"] = 2048
+    cfg["algo"]["phase"] = "adapt"
+    # cfg['algo']["phase"] = "finetune"
 
     if args.task is not None:
         with hydra.initialize(config_path="../cfg", job_name="eval", version_base=None):
             _cfg = hydra.compose(config_name="eval", overrides=[f"task={args.task}"])
         cfg["task"]["randomization"] = _cfg.task.randomization
         cfg["task"]["reward"] = _cfg.task.reward
+        if args.terrain:
+            cfg["task"]["terrain"] = _cfg.task.terrain
+        if args.command:
+            cfg["task"]["command"] = _cfg.task.command
     
-    app_launcher = AppLauncher(OmegaConf.to_container(cfg.app))
-    simulation_app = app_launcher.app
+    if args.play:
+        cfg["app"]["headless"] = False
+        cfg["task"]["num_envs"] = 16
+        cfg["export_policy"] = args.export
+        play(cfg)
+    else:
+        if args.video:
+            cfg["task"]["num_envs"] = 16
+            cfg["eval_render"] = True
+            cfg["app"]["enable_cameras"] = True
+            cfg["app"]["headless"] = False
+        eval(cfg)
 
-    env, agent, vecnorm = make_env_policy(cfg)
-
-    policy_eval = agent.get_rollout_policy("eval")
-    
-    keys = [
-        ("next", "stats"),
-        ("next", "done"), 
-        ("next", "reward"),
-    ]
-    info, trajs, stats = evaluate(env, policy_eval, seed=cfg.seed, keys=keys)
-    
-    path = os.path.join(os.path.dirname(__file__), f"{run.name}.pt")
-    torch.save(stats, path)
-
-    print(OmegaConf.to_yaml(info))
-    with open(os.path.join(os.path.dirname(__file__), f"{run.name}.yaml"), "w") as f:
-        f.write(OmegaConf.to_yaml(info))
-
-    env.close()
-    simulation_app.close()
 
 if __name__ == "__main__":
     main()
