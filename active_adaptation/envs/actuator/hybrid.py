a478d3bc8cdb372e5d50312b375ed5ca0b072e46
btx0424 <btx0424@outlook.com>
2024-11-29 02:14:01 +0800

    independent hybrid actuator rand

--- active_adaptation/envs/actuator/hybrid.py
+++ active_adaptation/envs/actuator/hybrid.py
@@ -13,12 +13,17 @@ class HybridActuator(ImplicitActuator):
 
     def __init__(self, cfg, *args, **kwargs):
         super().__init__(cfg, *args, **kwargs)
-        self.implicit = torch.zeros(self._num_envs, dtype=torch.bool, device=self._device)
+        self.implicit = torch.zeros(self._num_envs, self.num_joints, dtype=torch.bool, device=self._device)
         self.default_stiffness = self.stiffness[0].clone()
         self.default_damping = self.damping[0].clone()
     
     def reset(self, env_ids: torch.Tensor):
-        implicit = torch.rand(len(env_ids), device=self._device) < self.cfg.implicit_ratio
+        implicit = torch.where(
+            torch.rand(len(env_ids), 1, device=self._device) < self.cfg.homogeneous_ratio,
+            torch.rand(len(env_ids), 1, device=self._device),
+            torch.rand(len(env_ids), self.num_joints, device=self._device)
+        )  < self.cfg.implicit_ratio
+
         stiffness = self.default_stiffness.expand(len(env_ids), -1)
         damping = self.default_damping.expand(len(env_ids), -1)
         self.implicit[env_ids] = implicit
@@ -26,9 +31,8 @@ class HybridActuator(ImplicitActuator):
         # # these values are kept non-zero for computing the applied torque
         # self.stiffness[env_ids] = stiffness * (~implicit.unsqueeze(1))
         # self.damping[env_ids] = damping * (~implicit.unsqueeze(1))
-        
-        self.articulation.write_joint_stiffness_to_sim(stiffness * implicit.unsqueeze(1), self.joint_indices, env_ids)
-        self.articulation.write_joint_damping_to_sim(damping * implicit.unsqueeze(1), self.joint_indices, env_ids)
+        self.articulation.write_joint_stiffness_to_sim(stiffness * implicit, self.joint_indices, env_ids)
+        self.articulation.write_joint_damping_to_sim(damping * implicit, self.joint_indices, env_ids)
 
     def compute(
         self, control_action: ArticulationActions, joint_pos: torch.Tensor, joint_vel: torch.Tensor
@@ -37,12 +41,13 @@ class HybridActuator(ImplicitActuator):
         error_vel = control_action.joint_velocities - joint_vel
         self.computed_effort = self.stiffness * error_pos + self.damping * error_vel + control_action.joint_efforts
         self.applied_effort = self._clip_effort(self.computed_effort)
-        control_action.joint_efforts = self.applied_effort * (~self.implicit.unsqueeze(1))
+        control_action.joint_efforts = self.applied_effort * (~self.implicit)
         return control_action
 
 
 @configclass
 class HybridActuatorCfg(ImplicitActuatorCfg):
     implicit_ratio: float = 0.5
+    homogeneous_ratio: float = 0.5
     class_type = HybridActuator
 
