dcf17c3aceac6297ba6785062f45ba73ae77b962
btx0424 <btx0424@outlook.com>
2025-06-20 17:56:32 +0800

    GR1T2

--- active_adaptation/assets_mjcf/__init__.py
+++ active_adaptation/assets_mjcf/__init__.py
@@ -82,6 +82,90 @@ ROBOTS["g1_27dof"] = MJArticulationCfg(
     })
 )
 
+ROBOTS["gr1"] = MJArticulationCfg(
+    mjcf_path=os.path.join(PATH, "GR1T2", "GR1T2_nohand.xml"),
+    **json.load(open(os.path.join(PATH, "GR1T2", "GR1T2_nohand.json"))),
+    joint_symmetry_mapping=symmetry_utils.mirrored({
+        "left_hip_pitch_joint": (1, "right_hip_pitch_joint"),
+        "left_hip_roll_joint": (-1, "right_hip_roll_joint"),
+        "left_hip_yaw_joint": (-1, "right_hip_yaw_joint"),
+        "left_knee_pitch_joint": (1, "right_knee_pitch_joint"),
+        "left_ankle_pitch_joint": (1, "right_ankle_pitch_joint"),
+        "left_ankle_roll_joint": (-1, "right_ankle_roll_joint"),
+        "waist_yaw_joint": (-1, "waist_yaw_joint"),
+        "waist_roll_joint": (-1, "waist_roll_joint"),
+        "waist_pitch_joint": (1, "waist_pitch_joint"),
+        "left_shoulder_pitch_joint": (1, "right_shoulder_pitch_joint"),
+        "left_shoulder_roll_joint": (-1, "right_shoulder_roll_joint"),
+        "left_shoulder_yaw_joint": (-1, "right_shoulder_yaw_joint"),
+        "left_elbow_pitch_joint": (1, "right_elbow_pitch_joint"),
+        "left_wrist_yaw_joint": (1, "right_wrist_yaw_joint"),
+        "left_wrist_roll_joint": (-1, "right_wrist_roll_joint"),
+        "left_wrist_pitch_joint": (1, "right_wrist_pitch_joint"),
+        "head_yaw_joint": (-1,  "head_yaw_joint"),
+        "head_roll_joint": (-1, "head_roll_joint"),
+        "head_pitch_joint": (1, "head_pitch_joint"),
+    }),
+    spatial_symmetry_mapping=symmetry_utils.mirrored({
+        "base_link": "base_link",
+        "left_thigh_roll_link": "right_thigh_roll_link",
+        "left_thigh_yaw_link": "right_thigh_yaw_link",
+        "left_thigh_pitch_link": "right_thigh_pitch_link",
+        "left_shank_pitch_link": "right_shank_pitch_link",
+        "left_foot_pitch_link": "right_foot_pitch_link",
+        "left_foot_roll_link": "right_foot_roll_link",
+        "waist_yaw_link": "waist_yaw_link",
+        "waist_pitch_link": "waist_pitch_link",
+        "waist_roll_link": "waist_roll_link",
+        "head_roll_link": "head_roll_link",
+        "head_yaw_link": "head_yaw_link",
+        "head_pitch_link": "head_pitch_link",
+        "left_upper_arm_pitch_link": "right_upper_arm_pitch_link",
+        "left_upper_arm_roll_link": "right_upper_arm_roll_link",
+        "left_upper_arm_yaw_link": "right_upper_arm_yaw_link",
+        "left_lower_arm_pitch_link": "right_lower_arm_pitch_link",
+        "left_hand_yaw_link": "right_hand_yaw_link",
+        "left_hand_roll_link": "right_hand_roll_link",
+        "left_hand_pitch_link": "right_hand_pitch_link",
+    })
+)
+
+ROBOTS["g1_waist_unlocked"] = MJArticulationCfg(
+    mjcf_path=os.path.join(PATH, "g1_23dof", "g1_29dof_rev_1_0.xml"),
+    **json.load(open(os.path.join(PATH, "g1_23dof", "g1_29dof_rev_1_0.json"))),
+    joint_symmetry_mapping=symmetry_utils.mirrored({
+        "left_hip_pitch_joint": (1, "right_hip_pitch_joint"),
+        "left_hip_roll_joint": (-1, "right_hip_roll_joint"),
+        "left_hip_yaw_joint": (-1, "right_hip_yaw_joint"),
+        "left_knee_joint": (1, "right_knee_joint"),
+        "left_ankle_pitch_joint": (1, "right_ankle_pitch_joint"),
+        "left_ankle_roll_joint": (-1, "right_ankle_roll_joint"),
+        "waist_yaw_joint": (-1, "waist_yaw_joint"),
+        "waist_roll_joint": (-1, "waist_roll_joint"),
+        "waist_pitch_joint": (1, "waist_pitch_joint"),
+        "left_shoulder_pitch_joint": (1, "right_shoulder_pitch_joint"),
+        "left_shoulder_roll_joint": (-1, "right_shoulder_roll_joint"),
+        "left_shoulder_yaw_joint": (-1, "right_shoulder_yaw_joint"),
+        "left_elbow_joint": (1, "right_elbow_joint"),
+    }),
+    spatial_symmetry_mapping=symmetry_utils.mirrored({
+        "left_hip_pitch_link": "right_hip_pitch_link",
+        "left_hip_roll_link": "right_hip_roll_link",
+        "left_hip_yaw_link": "right_hip_yaw_link",
+        "left_knee_link": "right_knee_link",
+        "left_ankle_pitch_link": "right_ankle_pitch_link",
+        "left_ankle_roll_link": "right_ankle_roll_link",
+        "pelvis": "pelvis",
+        "torso_link": "torso_link",
+        "waist_yaw_link": "waist_yaw_link",
+        "waist_roll_link": "waist_roll_link",
+        "left_shoulder_pitch_link": "right_shoulder_pitch_link",
+        "left_shoulder_roll_link": "right_shoulder_roll_link",
+        "left_shoulder_yaw_link": "right_shoulder_yaw_link",
+        "left_elbow_link": "right_elbow_link",
+    })
+)
+
 ROBOTS["h2"] = MJArticulationCfg(
     mjcf_path=os.path.join(PATH, "h1_2", "h1_2_handless.xml"),
     **json.load(open(os.path.join(PATH, "h1_2", "h1_2_handless.json")))
