064578e09e321ad4d0b5f1091dbc3b3988c9baad
btx0424 <btx0424@outlook.com>
2025-07-24 20:55:09 +0800

    amend

--- active_adaptation/envs/mdp/commands/__init__.py
+++ active_adaptation/envs/mdp/commands/__init__.py
@@ -4,6 +4,8 @@ import importlib
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 for file in os.listdir(dir_path):
+    if file.startswith("_"):
+        continue
     if file.endswith(".py") and file != "__init__.py": # file module
         importlib.import_module(f".{file[:-3]}", __package__)
     elif os.path.isdir(os.path.join(dir_path, file)): # dir module
