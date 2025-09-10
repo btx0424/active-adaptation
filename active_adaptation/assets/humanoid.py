c6cd82940cb3a9385194aab530779d46cff1fcce
btx0424 <btx0424@outlook.com>
2025-09-10 23:42:01 +0800

    modify collisions

--- active_adaptation/assets/humanoid.py
+++ active_adaptation/assets/humanoid.py
@@ -382,7 +382,7 @@ G1_23DOF_CFG = ArticulationCfg( # no wrist pitch and yaw
 
 G1_WAIST_UNLOCKED_CFG = ArticulationCfg( # no wrist pitch and yaw
     spawn=sim_utils.UsdFileCfg(
-        usd_path=f"{ASSET_PATH}/unitree_description/urdf/g1/main/main.usd",
+        usd_path=f"{ASSET_PATH}/unitree_description/urdf/g1/main/main_capsule.usd",
         activate_contact_sensors=True,
         rigid_props=sim_utils.RigidBodyPropertiesCfg(
             disable_gravity=False,
